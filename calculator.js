bd837c77adaca9ad0e41f661f4df1df3b1b22807
unknown <sheyaaza33@gmail.com>
2023-02-16 10:36:34 +0530

    changes

--- calculator.js
+++ calculator.js
@@ -1,14 +1,22 @@
 var screen = document.getElementById('screen');
 
-function btnClick(value){
+function btnClick(value){               //namukk screenil oroo button
     screen.value += value;
 }
 
-function clearScreen(){
+//AC BUTTON
+function allClear(){
     screen.value = "";
 }
 
+//= BUTTON
 function findResult(){
-    var result = eval(screen.value)
+    var result = eval(screen.value)           //result is just a variable,we introduced here.
     screen.value = result;
+}                                       //in short we can also write  screen.value=eval(screen.value)
+
+// C BUTTON
+
+function clearScreen(){
+    screen.value=screen.value.slice(0,-1)
 }
